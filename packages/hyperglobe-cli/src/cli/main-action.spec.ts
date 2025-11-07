b5bab72901b373904be49832a7cb46290cc2899e
Malloc72P <scra1028@gmail.com>
2025-11-07 14:35:09 +0900

    Chore: high, mid 지도 확인

--- packages/hyperglobe-cli/src/cli/main-action.spec.ts
+++ packages/hyperglobe-cli/src/cli/main-action.spec.ts
@@ -3,9 +3,23 @@ import { describe, it } from 'vitest';
 import { mainAction } from './main-action';
 
 describe('main-action', () => {
-  it('디버거가 작동해야 한다', () => {
+  it('world-low', () => {
     const inputPath = resolve(join(__dirname, '/../../dummy/world-low.geo.json'));
     const outputPath = resolve(join(__dirname, '/../../dummy/world-low.hgm'));
+
+    mainAction({ input: inputPath, output: outputPath });
+  });
+
+  it('world-mid', () => {
+    const inputPath = resolve(join(__dirname, '/../../dummy/world-mid.geo.json'));
+    const outputPath = resolve(join(__dirname, '/../../dummy/world-mid.hgm'));
+
+    mainAction({ input: inputPath, output: outputPath });
+  });
+
+  it('world-high', () => {
+    const inputPath = resolve(join(__dirname, '/../../dummy/world-high.geo.json'));
+    const outputPath = resolve(join(__dirname, '/../../dummy/world-high.hgm'));
 
     mainAction({ input: inputPath, output: outputPath });
   });
--- packages/hyperglobe/pages/nations.tsx
+++ packages/hyperglobe/pages/nations.tsx
@@ -103,7 +103,9 @@ export function NationsDemo({ theme = 'blue' }: NationsDemoProps) {
     lock.current = true;
 
     (async function () {
-      const hgm = await fetch('/world-low.hgm').then((res) => res.blob());
+      //   const hgm = await fetch('/world-low.hgm').then((res) => res.blob());
+      //   const hgm = await fetch('/world-mid.hgm').then((res) => res.blob());
+      const hgm = await fetch('/world-high.hgm').then((res) => res.blob());
 
       setHgm(hgm);
       setLoading(false);
