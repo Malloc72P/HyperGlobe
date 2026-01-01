9233f7e4a98a7b434870715d84040ae3cdf7f182
Malloc72P <scra1028@gmail.com>
2026-01-02 08:44:10 +0900

    Fix: 스토리북 staticDIrs 설정 추가
    - 이미지, 데이터 안나오는 문제 있어서 해당 설정 추가함

--- packages/hyperglobe/.storybook/main.ts
+++ packages/hyperglobe/.storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
+  staticDirs: ['../public'],
   stories: [
     '../pages/**/*.mdx',
     '../pages/**/*.stories.tsx',
