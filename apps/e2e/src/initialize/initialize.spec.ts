fcb8ec87733cc01b10c2fe936f4812a7bb069b4a
Malloc72P <scra1028@gmail.com>
2026-01-06 08:49:10 +0900

    Chore: 예전 e2e 코드 주석처리, 스냅샷 제거

--- apps/e2e/src/initialize/initialize.spec.ts
+++ apps/e2e/src/initialize/initialize.spec.ts
@@ -1,20 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('E2E 테스트 동작 확인', async ({ page }) => {
-  await page.goto('http://localhost:6006/?path=/story/hyperglobe--getting-started');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/HyperGlobe/);
-});
-
 test('지구본 랜더링 테스트', async ({ page }) => {
-  await page.goto('http://localhost:6006/?path=/story/hyperglobe--getting-started');
-
-  const canvas = page
-    .locator('#storybook-preview-iframe')
-    .contentFrame()
-    .locator('#hyperglobe-canvas[data-is-rendered="true"]');
-
-  await expect(canvas).toBeVisible();
-  expect(await canvas.screenshot()).toMatchSnapshot('hyperglobe.png');
+  //   await page.goto('http://localhost:6006/?path=/story/hyperglobe--getting-started');
+  //   const canvas = page
+  //     .locator('#storybook-preview-iframe')
+  //     .contentFrame()
+  //     .locator('#hyperglobe-canvas[data-is-rendered="true"]');
+  //   await expect(canvas).toBeVisible();
+  //   expect(await canvas.screenshot()).toMatchSnapshot('hyperglobe.png');
 });
